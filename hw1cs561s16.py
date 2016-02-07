bfe705089b5c23b69fc8945a2760c6244732664f
Sagar Makwana <makwana_sagar@outlook.com>
2016-02-06 23:07:30 -0800

    -Issue #4 'Add Evaluation Function to GBFS' resolved (Entire GBFS changed) -Issue #5 'Write Simulation Code ' resolved

--- hw1cs561s16.py
+++ hw1cs561s16.py
@@ -66,22 +66,26 @@ def getRaidValue(currentBoardState, boardValues, player, iPosition, jPosition):
 
     return raidValue
 
-#Performs raid operation on the given raid position
-def performRaid(currentBoardState, gamePlayer , iRaidPosition , jRaidPosition):
-    enemyPlayer = getEnemy(gamePlayer)
-    currentBoardState[iRaidPosition][jRaidPosition] = gamePlayer
+#Performs raid and sneak operation on the given raid position
+def performOperation(currentBoardState, gamePlayer, iRaidPosition, jRaidPosition, operation):
+    if operation == 'raid':
+        enemyPlayer = getEnemy(gamePlayer)
+        currentBoardState[iRaidPosition][jRaidPosition] = gamePlayer
 
-    if (iRaidPosition-1 >= 0 and currentBoardState[iRaidPosition-1][jRaidPosition] == enemyPlayer):
-        currentBoardState[iRaidPosition-1][jRaidPosition] = gamePlayer
+        if (iRaidPosition-1 >= 0 and currentBoardState[iRaidPosition-1][jRaidPosition] == enemyPlayer):
+            currentBoardState[iRaidPosition-1][jRaidPosition] = gamePlayer
 
-    if (jRaidPosition-1 >= 0 and currentBoardState[iRaidPosition][jRaidPosition-1] == enemyPlayer):
-        currentBoardState[iRaidPosition][jRaidPosition-1] = gamePlayer
+        if (jRaidPosition-1 >= 0 and currentBoardState[iRaidPosition][jRaidPosition-1] == enemyPlayer):
+            currentBoardState[iRaidPosition][jRaidPosition-1] = gamePlayer
 
-    if (jRaidPosition+1 <=4  and currentBoardState[iRaidPosition][jRaidPosition+1] == enemyPlayer):
-        currentBoardState[iRaidPosition][jRaidPosition+1] = gamePlayer
+        if (jRaidPosition+1 <=4  and currentBoardState[iRaidPosition][jRaidPosition+1] == enemyPlayer):
+            currentBoardState[iRaidPosition][jRaidPosition+1] = gamePlayer
 
-    if (iRaidPosition+1 <= 4 and currentBoardState[iRaidPosition+1][jRaidPosition] == enemyPlayer):
-        currentBoardState[iRaidPosition+1][jRaidPosition] = gamePlayer
+        if (iRaidPosition+1 <= 4 and currentBoardState[iRaidPosition+1][jRaidPosition] == enemyPlayer):
+            currentBoardState[iRaidPosition+1][jRaidPosition] = gamePlayer
+
+    elif operation == 'sneak':
+        currentBoardState[iRaidPosition][jRaidPosition] = gamePlayer
 
 #Prints the log for minimax
 #@param boardPosition:Alphanumeric position on the board ex.A1,B2
@@ -124,108 +128,69 @@ def getBoardPosition(iPosition, jPosition):
     return str(chr(65+jPosition))+str(iPosition+1)
 
 #Returns a boolean whether the board is full or not.
-def isBoardFull (boarState):
+def isBoardFull (boardState):
     for i in range(0,5):
         for j in range(0,5):
             if not(isOccupied(boardState,i,j)):
                 return False
     return True
 
+#Gives the next move for the given player and algo
+def getNextMove(boardState, boardValues, player, playerAlgo, cutoff, traverseLogFile):
+
+    iNext = -1
+    jNext = -1
+
+    if playerAlgo == 1:
+        trash,iNext,jNext = GBFS(boardState,boardValues,player)
+    elif playerAlgo == 2:
+        trash,iNext,jNext = MINIMAX(boardState,boardValues,player,player,cutoff,0,-99,-99,traverseLogFile)
+    elif playerAlgo == 3:
+        trash,iNext,jNext = ABPrune(boardState,boardValues,player,player,cutoff,0,-99,-99,-float('inf'),float('inf'),traverseLogFile)
+
+    return iNext,jNext
+
 #----------------------------------------Algorithms--------------------------------------------------------
 
 #Algorithm for Greedy BFS algorithm
-def GBFS(gameTask, gamePlayer, gameEnemyPlayer,gameCutOff, boardValues, boardState):
+def GBFS(boardState, boardValues, gamePlayer):
 
-    #Initialization of Maximization Variables
-    maxValue = -1
-    maxMove = -1  # maxMove {1:Raid, 2:Sneak}
-    maxIPosition = -1
-    maxJPosition = -1
+    maxEvaluationValue = -float('inf')
+    iNextPosition = -1
+    jNextPosition = -1
 
-    print 'Before GBFS:'
-    print boardState
-
-    #Check the status of each position
     for i in range(0,5):
         for j in range(0,5):
-            #If the position is not occupied check if its sneakable.
+
             if (not(isOccupied(boardState,i,j))):
+                newBoardState = [eachRow[:] for eachRow in boardState]
+
                 if (isSneakable(boardState,gamePlayer,i,j)):
-                    if (int(boardValues[i][j]) > maxValue):
-                        maxValue = int(boardValues[i][j])
-                        maxIPosition = i
-                        maxJPosition = j
-                        maxMove = 2
+                    newBoardState[i][j] = gamePlayer
+                else:
+                    enemyPlayer = getEnemy(gamePlayer)
+                    newBoardState[i][j] = gamePlayer
 
-            # If the position is occupied by the player
-            elif (boardState[i][j] == gamePlayer):
+                    if (i-1 >= 0 and newBoardState[i-1][j] == enemyPlayer):
+                        newBoardState[i-1][j] = gamePlayer
 
-                if (i-1 >= 0 and not(isOccupied(boardState,i-1,j))):
-                    raidValue = getRaidValue(boardState,boardValues,gamePlayer,i-1,j)
-                    if (raidValue > maxValue):
-                        maxValue = raidValue
-                        maxIPosition = i-1
-                        maxJPosition = j
-                        maxMove = 1
+                    if (j-1 >= 0 and newBoardState[i][j-1] == enemyPlayer):
+                        newBoardState[i][j-1] = gamePlayer
 
-                if (j-1 >= 0 and not(isOccupied(boardState,i,j-1))):
-                    raidValue = getRaidValue(boardState,boardValues,gamePlayer,i,j-1)
-                    if (raidValue > maxValue):
-                        maxValue = raidValue
-                        maxIPosition = i
-                        maxJPosition = j-1
-                        maxMove = 1
+                    if (j+1 <=4  and newBoardState[i][j+1] == enemyPlayer):
+                        newBoardState[i][j+1] = gamePlayer
 
-                if (j+1 <= 4 and not(isOccupied(boardState,i,j+1))):
-                    raidValue = getRaidValue(boardState,boardValues,gamePlayer,i,j+1)
-                    if (raidValue > maxValue):
-                        maxValue = raidValue
-                        maxIPosition = i
-                        maxJPosition = j+1
-                        maxMove = 1
+                    if (i+1 <= 4 and newBoardState[i+1][j] == enemyPlayer):
+                        newBoardState[i+1][j] = gamePlayer
 
-                if (i+1 <= 4 and not(isOccupied(boardState,i+1,j))):
-                    raidValue = getRaidValue(boardState,boardValues,gamePlayer,i+1,j)
-                    if (raidValue > maxValue):
-                        maxValue = raidValue
-                        maxIPosition = i+1
-                        maxJPosition = j
-                        maxMove = 1
+                evaluationValue = getEvaluationValue(newBoardState,boardValues,gamePlayer)
 
+                if evaluationValue > maxEvaluationValue:
+                    maxEvaluationValue = evaluationValue
+                    iNextPosition = i
+                    jNextPosition = j
 
-    if (maxMove == 1): #Raid
-        #Do raid operation
-
-        boardState[maxIPosition][maxJPosition] = gamePlayer
-
-        if (maxIPosition-1 >= 0 and boardState[maxIPosition-1][maxJPosition] == gameEnemyPlayer):
-            boardState[maxIPosition-1][maxJPosition] = gamePlayer
-
-        if (maxJPosition-1 >= 0 and boardState[maxIPosition][maxJPosition-1] == gameEnemyPlayer):
-            boardState[maxIPosition][maxJPosition-1] = gamePlayer
-
-        if (maxJPosition+1 <=4  and boardState[maxIPosition][maxJPosition+1] == gameEnemyPlayer):
-            boardState[maxIPosition][maxJPosition+1] = gamePlayer
-
-        if (maxIPosition+1 <= 4 and boardState[maxIPosition+1][maxJPosition] == gameEnemyPlayer):
-            boardState[maxIPosition+1][maxJPosition] = gamePlayer
-
-        print 'Raid Operation performed at ',maxIPosition,' ',maxJPosition
-
-    elif (maxMove == 2):
-        #Do sneak operation
-        boardState[maxIPosition][maxJPosition] = gamePlayer
-
-        print 'Sneak operation performed',maxIPosition,' ',maxJPosition
-
-    print 'After GBFS:'
-    print boardState
-
-    outputFile = open('next_state.txt','w')
-
-    for i in range(0,5):
-        outputFile.write(''.join(boardState[i]))
-        outputFile.write('\n')
+    return maxEvaluationValue,iNextPosition,jNextPosition
 
 #Algorithm for Minimax
 #Constant parameters :{boardValue, gamePlayer, cutoffDepth, traverseLogFile }
@@ -399,87 +364,197 @@ def ABPrune (boardState, boardValues, gamePlayer, player, cutoffDepth, currentDe
 
 #----------------------------------------Input and Control--------------------------------------------------
 
+countFile = open('input.txt')
+decisionCount = 0
+for line in countFile:
+    decisionCount = decisionCount+1
+countFile.close()
 
 #1.Handling the Input
 
-#Initialisation of the variables
-gameTask = 0
-gamePlayer = 0
-gameEnemyPlayer = 0
-gameCutOff = 0
+#Single move Games
+if decisionCount == 13:
+    #Initialisation of the variables
+    gameTask = 0
+    gamePlayer = 0
+    gameEnemyPlayer = 0
+    gameCutOff = 0
 
-boardValues = []
-boardState = []
+    boardValues = []
+    boardState = []
 
-#Reading the input file
-inputFile = open('input.txt')
+    #Reading the input file
+    inputFile = open('input.txt')
 
-#Reading the game task player and cutoff
-gameTask  = inputFile.readline().strip()
-gameTask = int(gameTask)
-gamePlayer = inputFile.readline().strip()
-gameCutOff = inputFile.readline().strip()
-gameCutOff = int(gameCutOff)
-gameEnemyPlayer = getEnemy(gamePlayer)
+    #Reading the game task player and cutoff
+    gameTask  = inputFile.readline().strip()
+    gameTask = int(gameTask)
+    gamePlayer = inputFile.readline().strip()
+    gameCutOff = inputFile.readline().strip()
+    gameCutOff = int(gameCutOff)
+    gameEnemyPlayer = getEnemy(gamePlayer)
 
-#Reading the board grid values
-for i in range(0, 5):
-    line = inputFile.readline()
-    boardValues.append(line.split())
+    #Reading the board grid values
+    for i in range(0, 5):
+        line = inputFile.readline()
+        boardValues.append(line.split())
 
-#Reading the current board status
-for i in range(0, 5):
-    l = list(inputFile.readline().strip())
-    boardState.append(l)
+    #Reading the current board status
+    for i in range(0, 5):
+        l = list(inputFile.readline().strip())
+        boardState.append(l)
 
-inputFile.close()
 
-#2.Control
+    inputFile.close()
 
-if gameTask == 1:
-    print 'Greedy Best First Search in execution.'
-    GBFS(gameTask,gamePlayer,gameEnemyPlayer,gameCutOff,boardValues,boardState)
-elif gameTask == 2:
-    print 'Minimax Search in execution...'
+    #2.Control
+
+    if gameTask == 1:
+        print 'Greedy Best First Search in execution...'
+
+        nextStateFile = open('next_state.txt','w')
+
+        result,iNext,jNext = GBFS(boardState,boardValues,gamePlayer)
+
+        if iNext >= 0 and iNext <= 4 and jNext >= 0 and jNext <= 4:
+            print 'The position chosen is ',getBoardPosition(iNext,jNext),' with utility value of ',result,'.'
+            if (isSneakable(boardState,gamePlayer,iNext,jNext)):
+                performOperation(boardState, gamePlayer, iNext, jNext,'sneak')
+            else:
+                performOperation(boardState, gamePlayer, iNext, jNext,'raid')
+
+        for i in range(0,5):
+            nextStateFile.write(''.join(boardState[i]))
+            nextStateFile.write('\n')
+
+        nextStateFile.close()
+
+    elif gameTask == 2:
+        print 'Minimax Search in execution...'
+
+        traverseLogFile = open('traverse_log.txt','w')
+        nextStateFile = open('next_state.txt','w')
+        traverseLogFile.write("Node,Depth,Value\n")
+
+        result,iNext,jNext = MINIMAX(boardState,boardValues,gamePlayer,gamePlayer,gameCutOff,0,-99,-99,traverseLogFile)
+
+        if iNext >= 0 and iNext <= 4 and jNext >= 0 and jNext <= 4:
+            print 'The position chosen is ',getBoardPosition(iNext,jNext),' with utility value of ',result,'.'
+            if (isSneakable(boardState,gamePlayer,iNext,jNext)):
+                performOperation(boardState, gamePlayer, iNext, jNext,'sneak')
+            else:
+                performOperation(boardState, gamePlayer, iNext, jNext,'raid')
+
+        for i in range(0,5):
+            nextStateFile.write(''.join(boardState[i]))
+            nextStateFile.write('\n')
+
+        traverseLogFile.close()
+        nextStateFile.close()
+
+    elif gameTask == 3:
+        print 'Alpha-Beta Pruning Search in execution...'
+
+        traverseLogFile = open('traverse_log.txt','w')
+        nextStateFile = open('next_state.txt','w')
+        traverseLogFile.write("Node,Depth,Value,Alpha,Beta\n")
+
+        result,iNext,jNext = ABPrune(boardState,boardValues,gamePlayer,gamePlayer,gameCutOff,0,-99,-99,-float('inf'),float('inf'),traverseLogFile)
+
+        if iNext >= 0 and iNext <= 4 and jNext >= 0 and jNext <= 4:
+            print 'The position chosen is ',getBoardPosition(iNext,jNext),' with utility value of ',result,'.'
+            if (isSneakable(boardState,gamePlayer,iNext,jNext)):
+                performOperation(boardState, gamePlayer, iNext, jNext,'sneak')
+            else:
+                performOperation(boardState, gamePlayer, iNext, jNext,'raid')
+
+        for i in range(0,5):
+            nextStateFile.write(''.join(boardState[i]))
+            nextStateFile.write('\n')
+
+        traverseLogFile.close()
+        nextStateFile.close()
+
+else:
+
+    #1. Input
+
+    #Initialisation of the variables
+    gameTask = 0
+    player1 = 0
+    player2 = 0
+    player1Algo = 0
+    player2Algo = 0
+    player1Cutoff = 0
+    player2Cutoff = 0
+
+    boardValues = []
+    boardState = []
+
+    #Reading the input file
+    inputFile = open('input.txt')
+
+    gameTask = inputFile.readline().strip()
+    gameTask = int(gameTask)
+
+    player1 = inputFile.readline().strip()
+    player1Algo = inputFile.readline().strip()
+    player1Algo = int(player1Algo)
+    player1Cutoff = inputFile.readline().strip()
+    player1Cutoff = int(player1Cutoff)
+
+    player2 = inputFile.readline().strip()
+    player2Algo = inputFile.readline().strip()
+    player2Algo = int(player2Algo)
+    player2Cutoff = inputFile.readline().strip()
+    player2Cutoff = int(player2Cutoff)
+
+    #Reading the board grid values
+    for i in range(0, 5):
+        line = inputFile.readline()
+        boardValues.append(line.split())
+
+    #Reading the current board status
+    for i in range(0, 5):
+        l = list(inputFile.readline().strip())
+        boardState.append(l)
+
+    inputFile.close()
+
+
+    #2.Control
 
     traverseLogFile = open('traverse_log.txt','w')
-    nextStateFile = open('next_state.txt','w')
-    traverseLogFile.write("Node,Depth,Value\n")
+    traceStateFile = open('trace_state.txt','a')
+    player1Move = True
 
-    result,iNext,jNext = MINIMAX(boardState,boardValues,gamePlayer,gamePlayer,gameCutOff,0,-99,-99,traverseLogFile)
+    while (not(isBoardFull(boardState))):
+        if (player1Move == True):
+            iNext,jNext = getNextMove(boardState,boardValues,player1,player1Algo,player1Cutoff,traverseLogFile)
 
-    if iNext >= 0 and iNext <= 4 and jNext >= 0 and jNext <= 4:
-        print 'The position chosen is ',getBoardPosition(iNext,jNext),' with utility value of ',result,'.'
-        performRaid(boardState,gamePlayer,iNext,jNext)
+            if iNext >= 0 and iNext <= 4 and jNext >= 0 and jNext <= 4:
+                if (isSneakable(boardState,player1,iNext,jNext)):
+                    performOperation(boardState, player1, iNext, jNext,'sneak')
+                else:
+                    performOperation(boardState, player1, iNext, jNext,'raid')
 
-    for i in range(0,5):
-        nextStateFile.write(''.join(boardState[i]))
-        nextStateFile.write('\n')
+            player1Move = False
+
+        else:
+            iNext,jNext = getNextMove(boardState,boardValues,player2,player2Algo,player2Cutoff,traverseLogFile)
+
+            if iNext >= 0 and iNext <= 4 and jNext >= 0 and jNext <= 4:
+                if (isSneakable(boardState,player2,iNext,jNext)):
+                    performOperation(boardState, player2, iNext, jNext,'sneak')
+                else:
+                    performOperation(boardState, player2, iNext, jNext,'raid')
+
+            player1Move = True
+
+        for i in range(0,5):
+            traceStateFile.write(''.join(boardState[i]))
+            traceStateFile.write('\n')
+
+
 
     traverseLogFile.close()
-    nextStateFile.close()
-
-elif gameTask == 3:
-    print 'Alpha-Beta Pruning Search in execution...'
-
-    traverseLogFile = open('traverse_log.txt','w')
-    nextStateFile = open('next_state.txt','w')
-    traverseLogFile.write("Node,Depth,Value,Alpha,Beta\n")
-
-    result,iNext,jNext = ABPrune(boardState,boardValues,gamePlayer,gamePlayer,gameCutOff,0,-99,-99,-float('inf'),float('inf'),traverseLogFile)
-
-    if iNext >= 0 and iNext <= 4 and jNext >= 0 and jNext <= 4:
-        print 'The position chosen is ',getBoardPosition(iNext,jNext),' with utility value of ',result,'.'
-        performRaid(boardState,gamePlayer,iNext,jNext)
-
-    for i in range(0,5):
-        nextStateFile.write(''.join(boardState[i]))
-        nextStateFile.write('\n')
-
-    traverseLogFile.close()
-    nextStateFile.close()
-
-
-
-
-
